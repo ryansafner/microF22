a21aeb583809a52b65c28fb65bb9b6596d0f4dfa
Ryan Safner <ryansafner@gmail.com>
2022-09-11 15:50:21 -0400

    Add math review guide, 1.6 content

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -1,5 +1,5 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/1.4-slides.Rmd")
+rmarkdown::render("slides/1.5-slides.Rmd")
 
 # make-pdfs
 pagedown::chrome_print(input = "https://microf22.classes.ryansafner.com/slides/1.4-slides.html",
